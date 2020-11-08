0a1ef1af7a413eb5320b6ac855de84a399c3ccfb
Samber9891 <Slem0119@gmail.com>
2020-11-08 17:21:55 -0500

    populated product ArrayList

--- ecommercepage/src/main/java/com/tts/ecommercepage/controllers/MainController.java
+++ ecommercepage/src/main/java/com/tts/ecommercepage/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.tts.ecommercepage.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.tts.ecommercepage.models.Product;
@@ -19,8 +20,49 @@ public class MainController {
     @Autowired
     ProductService productService;
 
+    public void addNew() {
+List<Product> allProducts = productService.findAll();
+
+if(allProducts.isEmpty()) {
+    List<Product> newProducts = new ArrayList<Product>();
+
+    newProducts.add(new Product(null, 1,(float) 50.00, "Crown Ring", "Silver ring", "Kendra Scott", "Ring",  "image/Kendrascottcrown.jpg"));
+
+    newProducts.add(new Product(null, 1, (float) 150.00, "Gold trim hoop earings with silver lattice", "Drop Earrings", "Kendra Scott", "Earrings", "image/Kendrascottdrop.jpg"));
+
+    newProducts.add(new Product(null, 1, (float) 90.00, "Silver Ring", "Sliver ring", "Kendra Scott", "Ring", "image/Kendrascottring.jpg"));
+
+    newProducts.add(new Product(null, 1, (float) 120.00, "Silver Earrings with lattice print","Lattice Studs",  "Kendra Scott", "Earrings", "image/Kendrascottstud.jpg" ));
+
+    newProducts.add(new Product(null, 1, (float) 100.00, "Gold Drop Earrings","Drop Earrings",  "Madewell", "Earrings", "image/Madewelldrop.jpg" ));
+
+    newProducts.add(new Product(null, 1, (float) 30.00, "Goldhoop earrings with leaf detail","Leaf Earrings",  "Madewell", "Earrings", "image/Madewellearings.jpg" ));
+
+    newProducts.add(new Product(null, 1, (float) 50.00, "Mixed Ring Stack","Stack Rings",  "Madewell", "Rings", "image/Madewellstack.jpg" ));
+
+    newProducts.add(new Product(null, 1, (float) 50.00, "Gold Bar Ring","Bar Ring",  "Madewell", "Rings", "image/Madewellthick.jpg" ));
+
+    newProducts.add(new Product(null, 1, (float) 50.00, "gold chain necklace with coin pendent","Orbit Coin Pendant",  "Madewell", "Necklace", "image/Madewellcoin.jpg" ));
+
+    newProducts.add(new Product(null, 1, (float) 45.00, "Long change necklace with triangle detail","Triangle Necklace",  "Madewell", "Necklace", "image/Madewelltriangle.jpg" ));
+
+
+
+    for (Product product : newProducts) {
+        productService.save(product);
+    }
+       
+    } else {
+    // System.out.println("Cart Full"); 
+    // for (Product product : productService.findAll()) {
+    //     productService.deleteById(product.getId());
+    // }
+}
+    }
+    
     @GetMapping("/")
     public String main() {
+        addNew();
         return "main";
     }
 
@@ -36,7 +78,7 @@ public class MainController {
 
     @ModelAttribute("brands")
     public List<String> brands() {
-        return productService.findDistinctCategories();
+        return productService.findDistinctBrands();
     }
 
     @GetMapping("/filter")
